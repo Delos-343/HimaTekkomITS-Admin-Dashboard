2418ac97149f50f1cdc6ae1d5200e36421f25412
admiralkuznetsov45 <andafsul4@gmail.com>
2023-06-26 01:51:46 +0700

    styled title and tabs

--- src/pages/NewsApp.jsx
+++ src/pages/NewsApp.jsx
@@ -12,14 +12,14 @@ const HomePage = () => {
 
   return (
     <>
-      <div className="w-full py-10 justify-center items-middle font-semibold tracking-wider text-center text-5xl">
-        Dawe's TA ~ Dashboard
+      <div className="w-full py-10 justify-center items-middle font-bold tracking-wider text-center text-5xl text-blue-300">
+        Dawe's <span className="text-white"> Dashboard </span>
       </div>
       <div className="flex justify-center items-center mb-5 tracking-widest">
         <ul className="flex space-x-14">
           <li
             className={`cursor-pointer text-lg font-semibold ${
-              activeTab === 'notes' ? 'text-blue-500' : 'text-gray-500'
+              activeTab === 'notes' ? 'text-gray-100' : 'text-gray-400'
             }`}
             onClick={() => handleTabClick('notes')}
           >
@@ -27,7 +27,7 @@ const HomePage = () => {
           </li>
           <li
             className={`cursor-pointer text-lg font-semibold ${
-              activeTab === 'news' ? 'text-blue-500' : 'text-gray-500'
+              activeTab === 'news' ? 'text-gray-100' : 'text-gray-400'
             }`}
             onClick={() => handleTabClick('news')}
           >
